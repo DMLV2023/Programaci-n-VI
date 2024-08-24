6e47c5b5cddcfb4c1c2fa8faaddf0f37647b8701
DMLV2023 <133180235+DMLV2023@users.noreply.github.com>
2024-08-24 20:40:19 -0300

    clase-2v1

---
+++ Ejemploclase1/Persona.cs
@@ -0,0 +1,13 @@
+﻿namespace Ejemploclase1
+{
+    public class Persona
+    {
+        public string nombre { get; set; }
+
+        public string apellido { get; set; }
+
+        public string edad { get; set; }
+
+        public string profesion { get; set; }
+    }
+}
